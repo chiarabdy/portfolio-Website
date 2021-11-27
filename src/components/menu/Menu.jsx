4d13ea3f56345a43c2d4981d7048b92bbbbf718a
Chiar <chiar.evdi@gmail.com>
2021-11-27 04:02:33 +0100

    sideBar Menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,14 +1,16 @@
 import './menu.scss'
 
 export default function Menu({open, setOpen}) {
+    const activateMenu = ()=>setOpen(false)
+    
     return (
         <div className={'menu ' + (open && "active")}>
             <ul>
-                <li><a href="#intro">Home</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#testemonials">Testemonials</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li onClick={activateMenu}><a href="#intro">Home</a></li>
+                <li onClick={activateMenu}><a href="#portfolio">Portfolio</a></li>
+                <li onClick={activateMenu}><a href="#projects">Projects</a></li>
+                <li onClick={activateMenu}><a href="#testemonials">Testemonials</a></li>
+                <li onClick={activateMenu}><a href="#contact">Contact</a></li>
             </ul>
         </div>
     )
